65289137502999de44cc966dc9633cacfc206565
leonpack <anonymouszingme@gmail.com>
2024-07-17 17:49:14 +0700

    fix security issue

--- sub-node-1 test/redisconfig.js
+++ sub-node-1 test/redisconfig.js
@@ -3,9 +3,9 @@ const { Redis } = require("ioredis");
 class RedisConfig {
   constructor() {
     this.redis = new Redis({
-      host: "redis-16479.c252.ap-southeast-1-1.ec2.redns.redis-cloud.com",
-      port: 16479,
-      password: "5GkXt3GIXTxf7YQ3Q1NyXdF5nfO2Tt6b",
+      host: "your redis host",
+      port: 1234,
+      password: "your redis pass",
     });
   }
 
